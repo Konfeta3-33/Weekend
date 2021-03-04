d02c60e173092295b036a948d3dab7bf51a84ec5
EvgeniyaKa <56150618+EvgeniyaKa@users.noreply.github.com>
2021-03-04 22:43:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,24 +6,8 @@ import "./tailwind.output.css";
 import {Collaboration} from './components/collaboration/Collaboration';
 
 function App() {
-  // const routes = useRoutes();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/*example usage Tailwind below*/}
-        <p className="border-solid border-4 border-gray-600">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Footer />
     </div>
   );
