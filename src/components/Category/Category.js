f6d71669eac37bb68c93c35b4a2d30465b7d6328
Konfeta <konfeta333@gmail.com>
2021-03-26 11:08:51 +0300

    [fix] Fixed responsive site

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -8,7 +8,7 @@ const Category = ({ category, filteredCategory, toggle, toggleFavorites, favorit
   const { name, services } = category;
 
   return (
-    <div className="w-95 md:w-full xl:w-full mx-auto flex flex-col text-center -mb-2.5">
+    <div className="min-w-320 s:w-full sm:w-full md:w-full xl:w-full mx-auto flex flex-col text-center -mb-2.5">
       <div className="bg-category bg-no-repeat bg-cover bg-center">
         <div className="flex flex-wrap justify-center flex-col p-4">
           <div className="flex justify-center items-center w-60 mb-5 mx-auto mt-4">
