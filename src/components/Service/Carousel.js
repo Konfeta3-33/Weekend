ba4ec37d72b4687be3a2140a496c401c63a654d0
Konfeta <konfeta333@gmail.com>
2021-03-29 20:10:35 +0300

    [fix] Fixed scroll in Slider

--- src/components/Service/Carousel.js
+++ src/components/Service/Carousel.js
@@ -1,48 +1,40 @@
 import React from "react";
-import SwiperCore, { Pagination } from "swiper";
+import SwiperCore, { Pagination, Scrollbar } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
-import "../../pages/style.css";
+import "./style.css";
 import img from "./img/img.png";
 
-SwiperCore.use([Pagination]);
+SwiperCore.use([Pagination, Scrollbar]);
 const Carousel = ({ service }) => {
   const { Images } = service;
 
   return (
-    <div className="text-center">
-      <Swiper
-        spaceBetween={10}
-        slidesPerView={1}
-        // tag="service"
-        pagination={{ clickable: true }}
-        // centeredSlides
-        // centerInsufficientSlides
-        wrapperTag="div"
-        // onSlideChange={() => console.log("slide change")}
-        // onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide direction="horizontal"><img src={img}/></SwiperSlide>
-        <SwiperSlide direction="horizontal"><img src={img}/></SwiperSlide>
-        <SwiperSlide direction="horizontal"><img src={img}/></SwiperSlide>
-        {/*{Images.map((item, idx) => (*/}
-        {/*  <SwiperSlide*/}
-        {/*    zoom*/}
-        {/*    key={idx}*/}
-        {/*    tag="image"*/}
-        {/*    virtualIndex={idx}*/}
-        {/*    direction="horizontal"*/}
-        {/*  >*/}
-        {/*    <img*/}
-        {/*      src={item.src}*/}
-        {/*      alt={item.name}*/}
-        {/*      height="200"*/}
-        {/*      width="300"*/}
-        {/*    />*/}
-        {/*  </SwiperSlide>*/}
-        {/*))}*/}
-      </Swiper>
-    </div>
+    <Swiper
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      wrapperTag="div"
+    >
+      <SwiperSlide direction="horizontal"><img className="img" src={img}/></SwiperSlide>
+      <SwiperSlide direction="horizontal"><img className="img" src={img}/></SwiperSlide>
+      <SwiperSlide direction="horizontal"><img className="img" src={img}/></SwiperSlide>
+      {/*{Images.map((item, idx) => (*/}
+      {/*  <SwiperSlide*/}
+      {/*    zoom*/}
+      {/*    key={idx}*/}
+      {/*    tag="image"*/}
+      {/*    virtualIndex={idx}*/}
+      {/*    direction="horizontal"*/}
+      {/*  >*/}
+      {/*    <img*/}
+      {/*      src={item.src}*/}
+      {/*      alt={item.name}*/}
+      {/*      height="200"*/}
+      {/*      width="300"*/}
+      {/*    />*/}
+      {/*  </SwiperSlide>*/}
+      {/*))}*/}
+    </Swiper>
   );
 };
 
